2589db9ecdf74cc6e98d47e4f35c18e9765c8b9d
Trygvi Bech Arting <trygvi@gmail.com>
2019-06-18 16:47:44 +0200

    make reduce data available upon import

--- pysotope/__init__.py
+++ pysotope/__init__.py
@@ -32,6 +32,7 @@ from pysotope.data_reader import read_xls, read_json, parse_date
 from pysotope.invert import invert_data, summarise_data, exp_corr
 import pysotope.filelist as filelist
 from pysotope.diagrams import generate_cycleplots, generate_summaryplot
+from pysotope.run import reduce_data
 
 labeled = namedtuple('labeled', ['labels', 'data'])
 reduced = namedtuple('reduced', ['summary', 'data'])
